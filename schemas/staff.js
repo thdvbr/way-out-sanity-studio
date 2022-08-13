c7a3df81d8e5e398d9269761e77f6e828a8fa80d
thdvbr <soyoon.choi@code.berlin>
2022-08-13 17:40:22 +0200

    add warning on staff schema

--- schemas/staff.js
+++ schemas/staff.js
@@ -1,7 +1,7 @@
 import { BsPersonFill } from 'react-icons/bs';
 export default {
   name: 'staff',
-  title: 'Staff',
+  title: 'Staff (🚫 Do not add new names, only edit existing names 🚫)',
   type: 'document',
   icon: BsPersonFill,
   fields: [
