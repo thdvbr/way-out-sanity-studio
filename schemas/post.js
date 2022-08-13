3f43579f4a5a652299752f547d31079a7fb0599b
thdvbr <soyoon.choi@code.berlin>
2022-08-13 18:36:05 +0200

    add description on each field for post

--- schemas/post.js
+++ schemas/post.js
@@ -10,11 +10,13 @@ export default {
       name: 'title',
       title: 'Title',
       type: 'string',
+      description: 'Be sure to check the length and formatting for your title in the preview'
     },
     {
       name: 'subtitle',
       title: 'Subtitle',
-      type: 'string'
+      type: 'string',
+      description: 'Be sure to check the length and formatting for your subtitle in the preview'
     },
     {
       name: 'slug',
@@ -24,11 +26,13 @@ export default {
         source: 'title',
         maxLength: 96,
       },
+      description: 'Custom hyperlink text'
     },
     {
       name: 'mainImage',
       title: 'Main Image',
       type: 'image',
+      description: 'Banner image on front page + first image shown on article',
       options: {
         hotspot: true,
       },
@@ -39,7 +43,8 @@ export default {
           title: 'Caption',
           options: {
             isHighlighted: true
-          }
+          },
+          description: 'Text shown underneath the image on the article page'
         }
       ]
     },
@@ -50,38 +55,45 @@ export default {
       options: {
         hotspot: true,
       },
+      description: 'Image featured in the main grid + ‘more to read’ area'
     },
     {
       name: 'mainCategory',
       title: 'Main Category',
       type: 'reference',
-      to: {type: 'category'},
+      to: { type: 'category' },
+      description : 'Dropdown selects the menu category in which the article appears (Interview, Reviews, Radio)'
     },
     {
       name: 'subCategory',
       title: 'Sub Category',
       type: 'reference',
-      to: {type: 'subCategory'},
+      to: { type: 'subCategory' },
+      description : 'Subcategories can be custom added at any time. They appear at the top of the articles  in the ‘more to read’ area of the site.'
     },
     {
       name: 'publishedAt',
       title: 'Published at',
       type: 'datetime',
+      description: 'Day, Month, and Year of article post.'
     },
     {
       name: 'credits',
       title: 'Credits',
       type: 'string',
+      description :'[ Optional ] Open format entry for any credits (writing, photos, etc) '
     },
     {
       name: 'body',
       title: 'Body',
       type: 'blockContent',
+      description: 'Here is where you will enter the article text, photos, and links. \n• Intro Font: OptiArtCraft Bold\n • Speaker Font: Bold S Icon\n• Interview Question Font: Agrandir Narrow\n • Interviewee Response Font: Averia Serif\n • Centered Quote Font: Quote \n  • Artist Link Font - Normal'
     },
     {
       name: 'artistLink',
       title: 'Artist Link',
-      type: 'blockContent'
+      type: 'blockContent',
+      description :'Here is where you enter whatever custom links you want to include about the topic of your post (website, person’s social media, etc)   '
     }
   ],
 
