4c1f7bcea74480546683281affac2e1fb58f839e
thdvbr <soyoon.choi@code.berlin>
2022-08-15 12:34:35 +0200

    new deployment for env update on prod

--- schemas/page.js
+++ schemas/page.js
@@ -1,28 +1,28 @@
 import { ImFileEmpty } from 'react-icons/im';
 export default {
-    name: 'page',
-    type: 'document',
-    title: 'Page',
-    icon: ImFileEmpty,
-    fields: [
-        {
-            name: 'title',
-            type: 'string',
-            title: 'Title'
-        },
-        {
-            name: 'slug',
-            type: 'slug',
-            title: 'Slug',
-            option: {
-                source: 'title',
-                maxLength: 96,
-            }
-        },
-        {
-            name: 'body',
-            title: 'Body',
-            type: 'blockContent',
-        },
-    ]
-}
+  name: 'page',
+  type: 'document',
+  title: 'Page',
+  icon: ImFileEmpty,
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      option: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+  ],
+};
