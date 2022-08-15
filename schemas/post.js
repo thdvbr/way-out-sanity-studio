503cc107b112469b20b54d196691ddab6336e9ea
thdvbr <soyoon.choi@code.berlin>
2022-08-15 12:30:31 +0200

    new deployment

--- schemas/post.js
+++ schemas/post.js
@@ -1,4 +1,4 @@
-import { ImNewspaper } from 'react-icons/im'
+import { ImNewspaper } from 'react-icons/im';
 
 export default {
   name: 'post',
@@ -10,13 +10,15 @@ export default {
       name: 'title',
       title: 'Title',
       type: 'string',
-      description: 'Be sure to check the length and formatting for your title in the preview'
+      description:
+        'Be sure to check the length and formatting for your title in the preview',
     },
     {
       name: 'subtitle',
       title: 'Subtitle',
       type: 'string',
-      description: 'Be sure to check the length and formatting for your subtitle in the preview'
+      description:
+        'Be sure to check the length and formatting for your subtitle in the preview',
     },
     {
       name: 'slug',
@@ -26,7 +28,7 @@ export default {
         source: 'title',
         maxLength: 96,
       },
-      description: 'Custom hyperlink text'
+      description: 'Custom hyperlink text',
     },
     {
       name: 'mainImage',
@@ -42,11 +44,11 @@ export default {
           type: 'string',
           title: 'Caption',
           options: {
-            isHighlighted: true
+            isHighlighted: true,
           },
-          description: 'Text shown underneath the image on the article page'
-        }
-      ]
+          description: 'Text shown underneath the image on the article page',
+        },
+      ],
     },
     {
       name: 'previewImage',
@@ -55,61 +57,66 @@ export default {
       options: {
         hotspot: true,
       },
-      description: 'Image featured in the main grid + ‘more to read’ area'
+      description: 'Image featured in the main grid + ‘more to read’ area',
     },
     {
       name: 'mainCategory',
       title: 'Main Category',
       type: 'reference',
       to: { type: 'category' },
-      description : 'Dropdown selects the menu category in which the article appears (Interview, Reviews, Radio)'
+      description:
+        'Dropdown selects the menu category in which the article appears (Interview, Reviews, Radio)',
     },
     {
       name: 'subCategory',
       title: 'Sub Category',
       type: 'reference',
       to: { type: 'subCategory' },
-      description : 'Subcategories can be custom added at any time. They appear at the top of the articles  in the ‘more to read’ area of the site.'
+      description:
+        'Subcategories can be custom added at any time. They appear at the top of the articles  in the ‘more to read’ area of the site.',
     },
     {
       name: 'publishedAt',
       title: 'Published at',
       type: 'datetime',
-      description: 'Day, Month, and Year of article post.'
+      description: 'Day, Month, and Year of article post.',
     },
     {
       name: 'credits',
       title: 'Credits',
       type: 'string',
-      description :'[ Optional ] Open format entry for any credits (writing, photos, etc) '
+      description:
+        '[ Optional ] Open format entry for any credits (writing, photos, etc) ',
     },
     {
       name: 'body',
       title: 'Body',
       type: 'blockContent',
-      description: 'Here is where you will enter the article text, photos, and links. \n• Intro Font: OptiArtCraft Bold\n • Speaker Font: Bold S Icon\n• Interview Question Font: Agrandir Narrow\n • Interviewee Response Font: Averia Serif\n • Centered Quote Font: Quote \n  • Artist Link Font - Normal'
+      description:
+        'Here is where you will enter the article text, photos, and links. \n• Intro Font: OptiArtCraft Bold\n • Speaker Font: Bold S Icon\n• Interview Question Font: Agrandir Narrow\n • Interviewee Response Font: Averia Serif\n • Centered Quote Font: Quote \n  • Artist Link Font - Normal',
     },
     {
       name: 'artistLink',
       title: 'Artist Link',
       type: 'blockContent',
-      description :'Here is where you enter whatever custom links you want to include about the topic of your post (website, person’s social media, etc)   '
-    }
+      description:
+        'Here is where you enter whatever custom links you want to include about the topic of your post (website, person’s social media, etc)   ',
+    },
   ],
 
   preview: {
     select: {
       title: 'title',
       media: 'mainImage',
-      date: 'publishedAt'
+      date: 'publishedAt',
     },
     prepare(selection) {
-      const {title, date, media} = selection
-      return ( {
+      const { title, date, media } = selection;
+      return {
         media: media,
         title: title,
         subtitle: `${date.split('-')[0]}/${date.split('-')[1]}`,
-      })
+      };
     },
   },
-}
+};
